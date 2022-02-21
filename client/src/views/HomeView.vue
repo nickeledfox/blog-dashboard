490ad1b264f45bf34a2a6702005e36f0b249e711
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2022-02-20 20:12:41 -0800

    init

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row class="mb-4">
+      <el-button plain>Plain</el-button>
+      <el-button type="primary" plain>Primary</el-button>
+      <el-button type="success" plain>Success</el-button>
+      <el-button type="info" plain>Info</el-button>
+      <el-button type="warning" plain>Warning</el-button>
+      <el-button type="danger" plain>Danger</el-button>
+    </el-row>
+
+    <search style="width: 1em; height: 1em; margin-right: 8px" />
+    <el-icon><check /></el-icon>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import { Search, Edit, Check, Message, Delete } from '@element-plus/icons-vue';
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Search, Edit, Check, Message, Delete },
+};
 </script>
