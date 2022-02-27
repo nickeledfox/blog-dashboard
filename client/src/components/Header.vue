7f0d53d44a313a2e210599f5416c7e4940ed2f4f
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2022-02-26 19:35:48 -0800

    Add Header

---
+++ client/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-header>
+    <div class="toolbar">
+      <LogoText text="Blush & Bloom" />
+    </div>
+  </el-header>
+</template>
+
+<script lang="ts">
+import LogoText from './LogoText.vue';
+export default {
+  components: { LogoText },
+};
+</script>
---
+++ client/src/components/LogoText.vue
@@ -0,0 +1,16 @@
+<template>
+  <span class="main-title logo">{{ text }}</span>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['text'],
+};
+</script>
+
+<style lang="sass">
+.logo
+  font-size: 24px
+  line-height: 36px
+  padding: 0 22px
+</style>
