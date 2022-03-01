0195e9cd6afc99d3cc5c0b7ff7f291afae67aa16
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2022-03-01 15:44:11 -0800

    Set 404

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,9 +1,27 @@
 <template>
-  <h1 class="title">404</h1>
+  <el-container class="main-container not-found-page">
+    <div class="not-found-info">
+      <h1 class="title">404</h1>
+      <p>Oops! Page not found...</p>
+    </div>
+  </el-container>
 </template>
 
-<script lang="ts">
-export default {};
-</script>
+<script lang="ts" setup></script>
 
-<style></style>
+<style lang="sass">
+.not-found-page
+  height: 100vh
+  width: 100vw
+
+  .not-found-info
+    margin: 14rem auto
+    text-align: center
+
+    p
+      font-size: 2rem
+
+  .title
+    font-size: 5rem
+    padding-bottom: 2rem
+</style>
