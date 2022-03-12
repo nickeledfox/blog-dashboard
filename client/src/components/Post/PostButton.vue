f2e8e336b0e4409dc300808e1db7426374619739
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2022-03-12 15:04:00 -0800

    Moved button to separate component

---
+++ client/src/components/Post/PostButton.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { Right } from '@element-plus/icons-vue';
+const props = defineProps({
+  text: String,
+});
+</script>
+
+<template>
+  <el-button type="text" class="primary-button button">
+    {{ text }}
+    <el-icon class="post-card_footer__icon"><right /></el-icon>
+    <slot />
+  </el-button>
+</template>
+
+<style lang="sass">
+.primary-button
+  > span
+    padding: 3px 8px
+
+  min-height: auto
+  font-weight: 800
+  font-size: 16px
+  color: #000 !important
+  font-family: Avenir
+  outline: none
+
+  transition: all 0.5s
+  &:hover
+    > span
+    padding: 7px 8px
+    border: none
+    color: #fff !important
+    background: linear-gradient( 315deg, #00b712 0%,#79c951 74% )!important
+
+    .post-card_footer__icon
+      transform: scale(1.3)
+      font-weight: 900
+      right: 5px
+
+
+  &:active
+    .post-card_footer__icon
+      left: 4px
+</style>
